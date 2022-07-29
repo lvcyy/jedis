aa3b0a8aa81a1836f0257c385beadac049c11677
lvcyy <3235590755@qq.com>
2022-07-29 19:34:40 +0800

    master commit

--- src/main/java/com/wyh/jedis/GitText.java
+++ src/main/java/com/wyh/jedis/GitText.java
@@ -11,6 +11,7 @@ public class GitText {
         System.out.println("hello git2");
         System.out.println("hello git3");
         System.out.println("hello git4");
+        System.out.println("master test");
         System.out.println("hot-fix test !!");
     }
 }
