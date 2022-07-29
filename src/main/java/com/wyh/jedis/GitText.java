46f7df9c8e69c6c9d27f5f604a95b8246020f847
lvcyy <3235590755@qq.com>
2022-07-29 16:37:45 +0800

    第一次提交

---
+++ src/main/java/com/wyh/jedis/GitText.java
@@ -0,0 +1,12 @@
+package com.wyh.jedis;
+
+/**
+ * @author lvcyy
+ * @Package com.wyh.jedis
+ * @create 2022/7/29 16:36
+ */
+public class GitText {
+    public static void main(String[] args) {
+        System.out.println("hello git1");
+    }
+}
